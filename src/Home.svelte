7338985b2a04c812285d99516cb00d6f51a0e2e2
Jeremiah Trein <jeremiahtrein@gmail.com>
2019-09-09 14:23:50 +0700

    [master] adds a Home component for clearer routing

---
+++ src/Home.svelte
@@ -0,0 +1,14 @@
+<script>
+  import {searchText} from './store';
+  import NewsList from './NewsList.svelte';
+  import SearchInput from './SearchInput.svelte';
+</script>
+
+<h1>
+  Latest News for{' '}
+  <span aria-label="Flag of the United Kingdom" role="img">🇬🇧</span>
+  <span class="visually-hidden"> the United Kingdom</span>
+</h1>
+
+<SearchInput />
+<NewsList />
