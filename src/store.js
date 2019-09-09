97b0b77b950a84aaa50c872b424a34a0a386a784
Jeremiah Trein <jeremiahtrein@gmail.com>
2019-09-09 14:24:15 +0700

    [master] adds article and currentArticles to store

--- src/store.js
+++ src/store.js
@@ -1,3 +1,14 @@
 import {writable} from 'svelte/store';
 
+export const articles = writable([]);
+
+export const currentArticle = writable({
+  content: '',
+  description: '',
+  externalURL: '',
+  image: '',
+  sourceName: '',
+  title: '',
+});
+
 export const searchText = writable('');
