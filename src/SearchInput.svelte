9174adde836a9f1e2209606a0afc6956afcb2059
Jeremiah Trein <jeremiahtrein@gmail.com>
2019-09-09 11:49:49 +0700

    [master] adds a SearchInput component

---
+++ src/SearchInput.svelte
@@ -0,0 +1,30 @@
+<script>
+  import {searchText} from './store';
+
+  let searchValue = '';
+
+  function handleEnter(event) {
+    if (event.code === 'Enter') {
+      handleSearch(searchValue);
+    }
+  }
+
+  function handleSearch() {
+    // update the store
+    searchText.set(searchValue);
+  }
+
+  function handleValueChange(event) {
+    searchValue = event.target.value;
+  }
+</script>
+
+<label>
+  <span class="visually-hidden">Search articles</span>
+  <input
+    placeholder="Search"
+    on:keydown={handleEnter}
+    on:input={handleValueChange}
+    type="text" />
+  <button on:click={handleSearch}>Go</button>
+</label>
