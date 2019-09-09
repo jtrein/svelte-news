3a3369bb531193927ea9c3ba792c560115842073
Jeremiah Trein <jeremiahtrein@gmail.com>
2019-09-09 21:31:56 +0700

    [master] space formatting

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	transform: {
-		"\\.js$": "babel-jest",
-    	"\\.svelte$": "jest-transform-svelte"
+    "\\.js$": "babel-jest",
+    "\\.svelte$": "jest-transform-svelte"
 	},
 	moduleFileExtensions: ['js', 'json', 'svelte'],
 	bail: false,
