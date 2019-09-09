8bdf73a8de127299c99f544fa07b1152f4effb5b
Jeremiah Trein <jeremiahtrein@gmail.com>
2019-09-09 11:50:05 +0700

    NewsList data and messages now working well

--- src/NewsList.svelte
+++ src/NewsList.svelte
@@ -1,8 +1,58 @@
 <script>
-  // import {onMount} from 'svelte';
-  // import Article from './Article.svelte';
+  import {beforeUpdate, onMount} from 'svelte';
+  import {searchText} from './store';
+
+  let newsList = [];
+  let errorMessage = '';
+  let searchValue = '';
+  let searching = true;
+
+  // subscribe to store changes of `searchText`
+  searchText.subscribe(value => {
+    getNewsList(value);
+
+    searchValue = value;
+  });
   
-  // let newsList = [];
+  async function getNewsList(search = '') {
+    try {
+      searching = true;
+
+      const possibleSearchParam = search ? `?search=${encodeURIComponent(search.toLowerCase())}` : '';
+      const response = await fetch(`http://localhost:3000/latest/gb${possibleSearchParam}`);
+      const articles = await response.json();
+
+      newsList = articles;
+    } catch (error) {      
+      if (error) {
+        errorMessage = 'Something went awry while getting your news 😿.';
+      }
+    } finally {
+      searching = false;
+    }
+  }  
+
+  onMount(async () => {
+    getNewsList();
+  });
 </script>
 
-<p>Getting the latest news&hellip;</p>
+{#if searching && !errorMessage}
+  <p class="progress-text">Getting the latest news&hellip;</p>
+{/if}
+
+{#if !searching && !newsList.length}
+  <p>No articles were found matching "{searchValue}"</p>
+{/if}
+
+{#if errorMessage}
+  <p class="error__text">{errorMessage}</p>
+{/if}
+
+{#if newsList.length}
+  <ul>
+    {#each newsList as {description, publishedAt, title}}
+      <li class="newslist__row">{title}</li>
+    {/each}
+  </ul>
+{/if}
