6ec6243e07d4263ed37e7af60b252f1f184174e9
Jeremiah Trein <jeremiahtrein@gmail.com>
2019-09-09 14:22:48 +0700

    [master] updates NewsList handling of articles fetched

--- src/NewsList.svelte
+++ src/NewsList.svelte
@@ -1,28 +1,40 @@
 <script>
+  import { Link, navigate } from "svelte-routing";
   import {beforeUpdate, onMount} from 'svelte';
-  import {searchText} from './store';
+  import {articles, currentArticle, searchText} from './store';
 
-  let newsList = [];
+  let fetchedArticles = [];
   let errorMessage = '';
   let searchValue = '';
   let searching = true;
 
+  onMount(async () => {
+    if (searchText && fetchedArticles.length) return;
+    
+    getArticles();
+  });
+
   // subscribe to store changes of `searchText`
   searchText.subscribe(value => {
-    getNewsList(value);
+    getArticles(value);
 
     searchValue = value;
   });
   
-  async function getNewsList(search = '') {
+  articles.subscribe(value => {
+    fetchedArticles = value;
+  });
+  
+  async function getArticles(search = '') {
     try {
       searching = true;
 
       const possibleSearchParam = search ? `?search=${encodeURIComponent(search.toLowerCase())}` : '';
       const response = await fetch(`http://localhost:3000/latest/gb${possibleSearchParam}`);
-      const articles = await response.json();
+      const articlesJSON = await response.json();
 
-      newsList = articles;
+      // set store value for `articles`
+      articles.set(articlesJSON);
     } catch (error) {      
       if (error) {
         errorMessage = 'Something went awry while getting your news 😿.';
@@ -30,18 +42,32 @@
     } finally {
       searching = false;
     }
-  }  
+  }
 
-  onMount(async () => {
-    getNewsList();
-  });
+  function handleSetCurrentArticle(index) {
+    return function () {
+      const article = fetchedArticles[index - 1];
+
+      // set `currentArticle` in store
+      currentArticle.set({
+        content: article.content,
+        description: article.description,
+        externalURL: article.url,
+        image: article.urlToImage,
+        sourceName: article.source.name,
+        title: article.title,
+      });
+
+      navigate(`/${index}`);
+    };
+  }
 </script>
 
 {#if searching && !errorMessage}
   <p class="progress-text">Getting the latest news&hellip;</p>
 {/if}
 
-{#if !searching && !newsList.length}
+{#if !searching && !fetchedArticles.length}
   <p>No articles were found matching "{searchValue}"</p>
 {/if}
 
@@ -49,10 +75,16 @@
   <p class="error__text">{errorMessage}</p>
 {/if}
 
-{#if newsList.length}
+{#if fetchedArticles.length}
   <ul>
-    {#each newsList as {description, publishedAt, title}}
-      <li class="newslist__row">{title}</li>
+    {#each fetchedArticles as {description, publishedAt, title}, index}
+      <li class="article__row">
+        <Link
+          to={(index + 1).toString()}
+          on:click={handleSetCurrentArticle(index + 1)}>
+          {title}
+        </Link>
+      </li>
     {/each}
   </ul>
 {/if}
